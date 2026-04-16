adf3f2d1e3264f316952dee41bd9c34ce891cb29
PrabhaSumanGit <prabhasuman@trucksup.com>
2026-04-16 12:36:56 +0530

    commit

---
+++ LFODashboard/AuthServices/AuthServices_LFO.Model/Models/OtpVendorRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthServices_LFO.Model.Models
+{
+    public class OtpVendorRequest
+    {
+        public string mobile { get; set; }
+        public string otp { get; set; }
+        public string otptype { get; set; }
+    
+}
+}
--- LFODashboard/AuthServices/AuthServices_LFO/Program.cs
+++ LFODashboard/AuthServices/AuthServices_LFO/Program.cs
@@ -3,6 +3,7 @@ using AuthServices_LFO.BL.Interface;
 using AuthServices_LFO.DAL.Implemetation;
 using AuthServices_LFO.DAL.Interface;
 using DataAccessInterface;
+using HttpClientLib;
 using JwtAuthenticationManager;
 using Scalar.AspNetCore;
 
@@ -10,10 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IDataAccess, SqlDataAccess>();
+builder.Services.AddHttpClient<IHttpService, HttpService>();
 builder.Services.AddScoped<IAuthBL, AuthBL>();
 builder.Services.AddScoped<IAuthDAL, AuthDAL>();
 builder.Services.AddControllers();
 builder.Services.AddCustomJwtAuthentication(builder.Configuration);
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 

04eb5fd48f9848f4b482592763f6edd0be32dff4
sptrucksup01 <shivamprajapati@trucksup.com>
2026-04-27 19:09:49 +0530

    Manage Access Complete

---
+++ LFODashboard/ManageAccessService/ManageAccessService.BL/Implementation/PermissionBL.cs
@@ -0,0 +1,82 @@
+﻿using Common.Core;
+using ManageAccessService.BL.Interface;
+using ManageAccessService.DAL.Interface;
+using ManageAccessService.Model.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ManageAccessService.BL.Implementation
+{
+    public class PermissionBL : IPermissionBL
+    {
+        private readonly IPermissionDAL _permissionDAL;
+        public PermissionBL(IPermissionDAL permissionDAL)
+        {
+            _permissionDAL = permissionDAL;
+        }
+        public async Task<ApiResponse<string>> GetModules(PermissionRequest request)
+        {
+            try
+            {
+                if (request == null)
+                    return ApiResponse<string>.FailResponse("Invalid request", 400);
+
+                var result = await _permissionDAL.GetModules(request);
+
+                if (result == null || result.Rows.Count == 0)
+                    return ApiResponse<string>.FailResponse("No data found", 404);
+
+
+                var groupedData = result.AsEnumerable()
+                    .GroupBy(row => new
+                    {
+                        module_id = row.Field<int>("module_id"),
+                        module_name = row.Field<string>("module_name")
+                    })
+                    .Select(g => new
+                    {
+                        module_id = g.Key.module_id,
+                        module_name = g.Key.module_name,
+                        sub_modules = g
+                            .Where(x => x["id"] != DBNull.Value)
+                            .Select(x => new
+                            {
+                                id = x.Field<int>("id"),
+                                sub_module_name = x.Field<string>("sub_module_name")
+                            })
+                            .ToList()
+                    })
+                    .ToList();
+
+                return ApiResponse<string>.SuccessResponse(JsonConvert.SerializeObject(groupedData), "Data fetched successfully");
+            }
+            catch (Exception ex)
+            {
+                return ApiResponse<string>.FailResponse(ex.Message);
+            }
+        }
+
+        public async Task<ApiResponse<string>> GetFeatures(PermissionRequest request)
+        {
+            try
+            {
+                if (request == null)
+                    return ApiResponse<string>.FailResponse("Invalid request", 400);
+
+                var result = await _permissionDAL.GetFeatures(request);
+
+                if (result == null || result.Rows.Count == 0)
+                    return ApiResponse<string>.FailResponse("No data found", 404);
+
+                return ApiResponse<string>.SuccessResponse(JsonConvert.SerializeObject(result), "Data fetched successfully");
+            }
+            catch (Exception ex)
+            {
+                return ApiResponse<string>.FailResponse(ex.Message);
+            }
+        }
+    }
+}
---
+++ LFODashboard/ManageAccessService/ManageAccessService.DAL/Implementation/PermissionDAL.cs
@@ -0,0 +1,57 @@
+﻿using DataAccessInterface;
+using ManageAccessService.DAL.Interface;
+using ManageAccessService.Model.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ManageAccessService.DAL.Implementation
+{
+    public class PermissionDAL : IPermissionDAL
+    {
+        private readonly string _connStr;
+        private readonly IDataAccess _dataAccess;
+        public PermissionDAL(IConfiguration configuration, IDataAccess dataAccess)
+        {
+            _connStr = configuration.GetConnectionString("DefaultConnection");
+            _dataAccess = dataAccess;
+        }
+
+        public async Task<DataTable> GetModules(PermissionRequest request)
+        {
+            var parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@UserId", (object?)request.UserId ?? DBNull.Value),
+                new SqlParameter("@AccessType", (object?)request.AccessType ?? DBNull.Value),
+                new SqlParameter("@Action", "modules")
+            };
+
+            var result = await _dataAccess.ExecuteStoredProcedureAsync(_connStr, "usp_GetPermission",
+                parameters
+            );
+
+            return result;
+        }
+
+        public async Task<DataTable> GetFeatures(PermissionRequest request)
+        {
+            var parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@UserId", (object?)request.UserId ?? DBNull.Value),
+                new SqlParameter("@AccessType", (object?)request.AccessType ?? DBNull.Value),
+                new SqlParameter("@ModuleId", (object?)request.ModuleId ?? DBNull.Value),
+                new SqlParameter("@Action", "features")
+            };
+
+            var result = await _dataAccess.ExecuteStoredProcedureAsync(_connStr, "usp_GetPermission",
+                parameters
+            );
+
+            return result;
+        }
+
+    }
+}
